91151dfbe8e41f7df7a30fc544db6b503b990d95
Vedprakash Kumar <131158289+Vedprakash321@users.noreply.github.com>
2024-04-28 20:41:15 +0000

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,30 @@
+// App.js
 import React, { useState } from 'react';
-import './styles/App.css';
+import '../styles/App.css';
 
 const App = () => {
   const [markdown, setMarkdown] = useState('');
+
+  const convertToHTML = (markdownText) => {
+    return { __html: markdownText.replace(/(?:\r\n|\r|\n)/g, '<br>') };
+  };
 
   const handleChange = (event) => {
     setMarkdown(event.target.value);
   };
 
   return (
-    <div className="app">
-      <textarea
-        className="textarea"
-        value={markdown}
-        onChange={handleChange}
-        placeholder="Write markdown here..."
-      ></textarea>
-      <div className="preview">
-        {markdown && (
-          <>
-            <h1>Markdown Preview</h1>
-            <div
-              className="markdown-content"
-              dangerouslySetInnerHTML={{ __html: markdown }}
-            ></div>
-          </>
-        )}
-      </div>
-    </div>
+    <div className="preview">
+    {markdown && (
+      <>
+        <h1>Markdown Preview</h1>
+        <div
+          className="markdown-content"
+          dangerouslySetInnerHTML={{ __html: markdown }}
+        ></div>
+      </>
+    )}
+  </div>
   );
 };
 
